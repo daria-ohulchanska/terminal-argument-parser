fb9de6d58d9f002c841a13a6366b774b23f58e5f
Daria Ohulchanska <daria.ohulchanska@gmail.com>
2024-09-09 14:02:13 +0200

    Fix logger.

--- TerminalArgumentParser/Program.cs
+++ TerminalArgumentParser/Program.cs
@@ -2,23 +2,42 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using TerminalArgumentParser.Interfaces;
+using TerminalArgumentParser.Models;
 using TerminalArgumentParser.Services.Parsers;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var host = Host.CreateDefaultBuilder(args)
+        var host = CreateHostBuilder(args).Build();
+
+        var parser = host.Services.GetRequiredService<IParser<Dictionary<string, string>>>();
+        
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning)
+                .AddConsole();
+        });
+
+        ILogger logger = loggerFactory.CreateLogger<Program>();
+
+        var parsed = parser.Parse(args);
+        logger.LogInformation("Parsed arguments: {Arguments}", parsed);
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
             .ConfigureServices((_, services) =>
             {
                 services.AddScoped<IParser<Dictionary<string, string>>, ArgumentParser>();
+                services.AddScoped<IParser<Arguments>, FixedArgumentParser<Arguments>>();
             })
             .ConfigureLogging(logging =>
             {
                 logging.ClearProviders();
-                logging.AddConsole();
-                logging.SetMinimumLevel(LogLevel.Information);
-            })
-            .Build();
+                logging.AddSimpleConsole(options => options.IncludeScopes = true);
+                logging.AddEventLog();
+            });
     }
-}
